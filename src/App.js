8e20aba6c664298190182b18e62092cff834fdd6
Aitan <aitan.mammadova@mail.ru>
2021-12-21 23:50:29 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+        <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
