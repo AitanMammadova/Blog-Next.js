c250d212938db254af9aed0e6f2fecde1379f561
Aitan <aitan.mammadova@mail.ru>
2021-12-22 00:43:02 +0400

    Click events

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,17 @@
 const Home = () => {
+  const handleClick=(e)=>{
+    console.log("hello worlds",e);
+  }
+  
+  const clickAgain=(name,e)=>{
+    console.log('hello ' + name,e.target);
+  }
     return (
       <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e)=>clickAgain('aitan',e)}>Click me Again</button>
       </div>
     );
   }
-   
   export default Home;
