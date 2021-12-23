7fb3add7d4674783efab0fbc474875436bce5288
Aitan <aitan.mammadova@mail.ru>
2021-12-24 00:11:03 +0400

    Props

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,14 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 const Home = () => {
-  const [name,setName] = useState('aitan');
-  const [age,setAge] = useState(19);
-  const handleClick=()=>{
-    setName('ali');
-    setAge(20);
-  }
+    const [blogs,setBlogs]=useState([
+      {title:"My new websites", body:'lorem ipsum...', author:"mario", id:1},
+      {title:"Welcome party!", body:'lorem ipsum...', author:"yoshi", id:2},
+      {title:"Web dev top tips", body:'lorem ipsum...', author:"mario", id:3}
+    ]);
     return (
       <div className="home">
-        <h2>Homepage</h2>
-        <p>{name} is {age} years old.</p>
-        <button onClick={handleClick}>Click me</button>
+        <BlogList blogs = {blogs} title ='All Blogs'/>;
       </div>
     );
   }
