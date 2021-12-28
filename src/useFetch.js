2cbd6e8845d3c34d36443bf9bff96e0ae33d2383
Aitan <aitan.mammadova@mail.ru>
2021-12-28 23:32:25 +0400

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch =(url)=>{
     const [error, setError] = useState(null);
 
     useEffect(()=>{
+        const abortCont = new AbortController();
         setTimeout(() => {
-          fetch(url)
+          fetch(url, {signal:abortCont.signal})
     
           .then(res=>{
             if(!res.ok){
@@ -22,10 +23,16 @@ const useFetch =(url)=>{
           })
           
           .catch(err=>{
-            setError(err.message);
-            setIsPending(false)
+              if(err.name==='AbortError'){
+                  console.log('fetch aborted')
+              }else{
+                setError(err.message);
+                setIsPending(false);
+              }
+            
           })
         }, 1000);
+        return ()=>abortCont.abort();
       },[url]);
       
       return {data,isPending,error};
