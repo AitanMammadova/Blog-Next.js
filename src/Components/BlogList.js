962b5c1315810bc0b942bc1555943ae961fd9806
Aitan <aitan.mammadova@mail.ru>
2021-12-29 23:08:23 +0400

    BlogDetails

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -7,10 +7,11 @@ const BlogList = ({blogs,title}) => {
             <h2>{title}</h2>
             {blogs.map((blog)=>(
             <div className="blog-preview" key={blog.id}>
-                <Link to={`/blogs/ ${blog.id}`}>
+                <Link to={`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                 </Link>
+                
             </div>
         ))}
         </div>
