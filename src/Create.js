6119a7ae5dbb2a82305e6508622181d85e11b884
Aitan <aitan.mammadova@mail.ru>
2021-12-30 22:23:08 +0400

    Author with Input

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import {useHistory} from 'react-router-dom'
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('mario');
+    const [author, setAuthor] = useState('');
     const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
@@ -41,12 +41,11 @@ const Create = () => {
                 onChange={(e)=>setBody(e.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
-                <select
+                <input 
+                type="text" 
                 value={author}
-                onChange={(e)=>setAuthor(e.target.value)}>
-                    <option value ="mario">mario</option>
-                    <option value = "yoshi">yoshi</option>
-                </select>
+                onChange={(e)=>setAuthor(e.target.value)}
+                />
                 {!isPending && <button>Add Blog</button>}
                 {isPending && <button disabled>Adding Blog</button>}
 
